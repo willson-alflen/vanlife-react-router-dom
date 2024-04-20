c3cc2610715cd8caf1e5ea15fa6386472727e4e9
willson-alflen <willson.alflen@gmail.com>
2024-04-20 08:52:58 -0300

    Added .env file and updated api.js to use environment variables

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,8 @@
+import { node } from 'prop-types'
+
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
--- src/api/index.js
+++ src/api/index.js
@@ -21,13 +21,13 @@ import {
 } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBJbRfBLeXbhNNksDlO5v_uTgMpRZhiAvI',
-  authDomain: 'vanlife-28669.firebaseapp.com',
-  databaseURL: 'https://vanlife-28669-default-rtdb.firebaseio.com',
-  projectId: 'vanlife-28669',
-  storageBucket: 'vanlife-28669.appspot.com',
-  messagingSenderId: '771541759606',
-  appId: '1:771541759606:web:09ea22c1e9b72b7a945157',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
